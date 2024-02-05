a68d15e56cea18165c757e733de06a740d561434
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-05 13:14:37 +0530

    feat: Add Button with styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { FcGoogle } from "react-icons/fc";
 import { useState, useEffect } from 'react'
 
 const App = () => {
     const [isValid, setIsValid] = useState(false)
     const [isOpacity, setIsOpacity] = useState(false)
+    const [googlePing, setGooglePing] = useState(false)
+
+    const handleClickPing = () => {
+        setGooglePing(true)
+        setTimeout(() => {
+            setGooglePing(false)
+        }, 1000);
+    }
 
     useEffect(() => {
         const animationPause = setTimeout(() => {
@@ -31,8 +40,11 @@ const App = () => {
                         absolute flex flex-col items-center justify-center top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4  overflow-hidden rounded-3xl w-login h-login bg-white bg-tasks bg-center bg-no-repeat 
                         transition-opacity duration-1000 ease-in ${isOpacity ? 'opacity-100' : 'opacity-0'}
                     `} >
-                    <div className='bg-gradient-to-t from-gray-700 via-gray-450 bg-opacity-40 w-login h-login'>
-                        {/* Login Stuff */}
+                    <div className='flex flex-row justify-center items-center bg-gradient-to-t  from-gray-700 via-gray-450 bg-opacity-40 w-login h-login'>
+                        <div className='flex flex-col justify-between items-center pt-96'>
+                            <button className='w-72 p-2 m-2 bg-gray-200 border-gray-200 hover:bg-gray-950 hover:border-gray-950  bg-opacity-30 hover:bg-opacity-90 font-extralight  hover:text-white rounded-full'>Sign In</button>
+                            <button onClick={handleClickPing} className=' flex flex-row justify-center items-center gap-2 w-72 p-2 m-2 bg-gray-200 text-gray-900 opacity-70 hover:opacity-100  font-extralight border-gray-200 rounded-full'><FcGoogle className={`${googlePing ? 'animate-ping' : 'animate-none'}`} />Sign In with Google</button>
+                        </div>
                     </div>
                 </div>
             </div>
