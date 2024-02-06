235915c9e5e206ae58808d89b2b2437d8dba6229
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-07 04:44:46 +0530

    feat: Add required and a name attribute with a name prop

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,4 +1,5 @@
 const Input = ({
+    name,
     children,
     type,
     placeholder,
@@ -8,7 +9,7 @@ const Input = ({
     return (
         <div>
             <h3 className="text-xs">{children}</h3>
-            <input type={type} onClick={onClick} placeholder={placeholder} className={`bg-gray-200 border-gray-200 opacity-70 text-gray-950 w-72 p-1 pl-2 mb-1 mt-1 placeholder:text-xs rounded-lg`}>
+            <input required name={name} type={type} onClick={onClick} placeholder={placeholder} className={`bg-gray-200 border-gray-200 opacity-70 text-gray-950 w-72 p-1 pl-2 mb-1 mt-1 placeholder:text-xs rounded-lg`}>
             </input>
         </div>
     )
