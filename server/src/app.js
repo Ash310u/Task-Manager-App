414fdd6cc2f8b51c3239bacde49e07477b37d010
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-07 13:15:50 +0530

    feat: Add cors node module to set 'Access-Control-Allow-Origin' for my frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,16 @@
 // I just want Express application before listen to be called, To make request using supertest. And that's why I create app.js and export it.
 
 const express = require("express");
+const cors = require("cors")
 require("./db/mongoose");
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task');
 
 const app = express();
 
+app.use(cors({
+    origin:'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(taskRouter)
 app.use(userRouter);
