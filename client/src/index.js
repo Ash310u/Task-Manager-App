42a83733b2fce03db3379f41b23245bd5eb50f9f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-07 13:59:52 +0530

    feat: set react-redux store Provider with store prop

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux"
 import App from './App'
 import './index.css'
 import { NavigationProvider } from "./context/navigation";
+import { store } from "./store";
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el);
 
 root.render(
     <NavigationProvider>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </NavigationProvider>
 )
