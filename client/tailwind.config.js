89102865deccc5e86b7b11aa3fa78a966733aeb7
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-08 18:30:36 +0530

    refactor: remove width height

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,11 +9,9 @@ module.exports = {
         'login': "url('/src/assets/loginPage/backgrounds/bg.jpg')"
       },
       width : {
-        'login-moon': '50%',
         'login': '60%',
       },
       height : {
-        'login-moon': '70%',
         'login': '60%',
       },
       backdropBlur: {
