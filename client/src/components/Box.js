c1022f18853931f4891aaccb8aeaf202efb1c312
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-09 06:19:14 +0530

    feat: Add Edit a Threedots btns inside box comp

--- client/src/components/Box.js
+++ client/src/components/Box.js
@@ -1,9 +1,22 @@
+import { BiSolidEditAlt } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+
 const Box = ({ children }) => {
     return (
-        <div className='max-w-min  max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3 m-3'>
-            <p className='text-white w-80 break-words'>
+        <div className={`max-w-80 max-h-min flex flex-row bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3 m-3 gap-2 overflow-hidden group }`}>
+            <p className='text-gray-50 w-80 subpixel-antialiased break-words'>
                 {children}
             </p>
+            <div className="absolute right-5 hidden group-hover:block text-gray-50">
+                <div className="flex flex-row justify-center items-center text-xl rounded-lg">
+                    <div className=" bg-black p-1.5 hover:bg-gray-800  rounded-l-lg">
+                        <BiSolidEditAlt />
+                    </div>
+                    <div className=" bg-black p-1.5 hover:bg-gray-800  rounded-r-lg">
+                        <BsThreeDots />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/pages/DashboardPage.js
+++ client/src/components/pages/DashboardPage.js
@@ -3,7 +3,7 @@ import Box from "../Box"
 const DashboardPage = () => {
     return (
         <div className="w-screen h-screen bg-black bg-opacity-70 backdrop-blur text-white">
-            <Box>Sam Altman finishes up "Ideas, Products, Teams and Execution" by covering Team and Execution, in Lecture 2 of How to Start a startup.</Box>
+            <Box>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Box>
         </div>
     )
 }
