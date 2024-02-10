8c8b0931a4e7280f5bd06cd81eca5aef116c0076
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-10 14:08:04 +0530

    feat: add a Panel comp with some boilerplate

--- client/src/components/pages/DashboardPage.js
+++ client/src/components/pages/DashboardPage.js
@@ -1,9 +1,10 @@
-import Box from "../Box"
+// import Box from "../Box"
+import Panel from "../Panel"
 
 const DashboardPage = () => {
     return (
-        <div className="w-screen h-screen bg-black bg-opacity-70 backdrop-blur text-white">
-            <Box>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Box>
+        <div className="backdrop-blur text-white p-10 ">
+            <Panel></Panel>
         </div>
     )
 }
