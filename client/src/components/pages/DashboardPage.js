242b0f0379edc905200122409fac148357f98568
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-10 23:48:36 +0530

    style testing: add some gap and test styling

--- client/src/components/pages/DashboardPage.js
+++ client/src/components/pages/DashboardPage.js
@@ -3,7 +3,7 @@ import Panel from "../Panel"
 
 const DashboardPage = () => {
     return (
-        <div className="w-screen h-screen backdrop-blur text-white p-10 overflow-auto">
+        <div className="w-screen h-screen backdrop-blur flex flex-row flex-4 gap-10 text-white p-10 overflow-auto">
             <Panel header='For Exam'>
                 <Box>hello world</Box>
                 <Box>hello world</Box>
@@ -21,6 +21,251 @@ const DashboardPage = () => {
                 <Box>hello world</Box>
                 <Box>hello world</Box>
                 <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For somthing'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For Exam'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For somthing'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For Exam'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For somthing'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For Exam'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For somthing'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For Exam'>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+                <Box>hello world</Box>
+            </Panel>
+            <Panel header='For somthing'>
                 <Box>hello world</Box>
                 <Box>hello world</Box>
                 <Box>hello world</Box>
