a4175f89bdcff533e1925131a8018b7114a37546
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-15 01:17:51 +0530

    feat: Add styling to hide the scroll bar using webkit

--- client/src/components/pages/DashboardPage.js
+++ client/src/components/pages/DashboardPage.js
@@ -3,10 +3,69 @@ import Panel from "../Panel"
 
 const DashboardPage = () => {
     return (
-        <div className="w-screen h-screen backdrop-blur flex flex-row flex-4 text-white p-10 overflow-auto">
-            <Panel header='For Exam'>
-                <Box>hello world</Box>
-            </Panel>
+        <div className="w-screen h-screen flex flex-col p-10 backdrop-blur text-white overflow-auto">
+            <div className="m-5">
+            </div>
+            <div className="flex flex-row flex-4 gap-10 m-10 overflow-auto">
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+                <Panel header='For Exam'>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                    <Box>hello world</Box>
+                </Panel>
+            </div>
+            {/* Style for hiding the scroll bar */}
+            <style>
+                {`
+                    .overflow-auto::-webkit-scrollbar {
+                        width: 0 !important;
+                    }
+                    .overflow-auto {
+                        scrollbar-width: none;
+                        -ms-overflow-style: none;
+                    }
+                `}
+            </style>
         </div>
     )
 }
