6fdadc1891cb4313c3626bbf59ed66b4a514b0cd
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-15 23:43:00 +0530

    feat: update the ControlPanel.js & add StatusBox.js comp

--- client/src/components/ControlPanel.js
+++ client/src/components/ControlPanel.js
@@ -1,15 +1,44 @@
 import { MdAdd } from "react-icons/md";
+import StatusBox from "./StatusBox";
 
 const ControlPanel = () => {
     return (
-        <div className="flex flex-row justify-between items-center bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-5">
-            <div className="p-3">
-                more Controls
+        <div className="flex flex-row items-center justify-around bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-4 gap-4">
+            <div className="pr-3 pl-3 pt-1 pb-1 flex flex-row gap-2 overflow-auto items-center bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg">
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+                <StatusBox/>
+               
             </div>
-            <div className="flex flex-row items-center gap-1 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg pt-2 pb-2 pr-3 pl-3" >
+            <div className="flex flex-row items-center  gap-1 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg pt-2 pb-2 pr-3 pl-3 " >
                 <button>New</button>
-                <MdAdd className="text-lg"/>
+                <MdAdd className="text-lg" />
             </div>
+            {/* Style for hiding the scroll bar */}
+            <style>
+                {`
+                    .overflow-auto::-webkit-scrollbar {
+                        width: 0 !important;
+                    }
+                    .overflow-auto {
+                        scrollbar-width: none;
+                        -ms-overflow-style: none;
+                    }
+                `}
+            </style>
         </div>
     )
 }
---
+++ client/src/components/StatusBox.js
@@ -0,0 +1,14 @@
+const StatusBox = () => {
+    return (
+        <div className="flex flex-col min-w-32">
+            <div className="">
+                Topic
+            </div>
+            <div className="max-h-6">
+                CompletedStatus
+            </div>
+        </div>
+    )
+}
+
+export default StatusBox;
