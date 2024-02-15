589397f87719b9b7c0b79695c405f2f18d01fbce
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-15 23:41:39 +0530

    feat: set up the topicApi reducer and middleware and export it from index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,18 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { userApi } from "./apis/userApi";
+import { topicApi } from "./apis/topicApi";
 
 const store = configureStore({
     reducer: {
         [userApi.reducerPath]: userApi.reducer,
+        [topicApi.reducerPath]: topicApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
-            .concat(userApi.middleware);
+            .concat(userApi.middleware)
+            .concat(topicApi.middleware)
     }
 })
 
 setupListeners(store.dispatch);
 
-export { useCreateAccountMutation } from './apis/userApi'
+export { useCreateAccountMutation } from './apis/userApi';
+export { useCreateTopicMutation } from './apis/topicApi';
 export { store };
