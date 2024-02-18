2241c3218f82aa1e21c3fb16bfa72142876161a4
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-18 21:39:44 +0530

    refactor: refactor the setInput prop and set required

--- client/src/components/SmallComps/EditInput.js
+++ client/src/components/SmallComps/EditInput.js
@@ -6,34 +6,32 @@ function EditInput({ setIsInput }) {
     const dispatch = useDispatch()
 
     const [value, setValue] = useState('');
-    const [ createTopic, results] = useCreateTopicMutation()
+    const [createTopic, results] = useCreateTopicMutation()
 
-     const authToken = useSelector(state => {
+    const authToken = useSelector(state => {
         return state.userData.user?.token;
     })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const topic = {title: e.target.topic.value}
+        const topic = { title: e.target.topic.value }
         createTopic({ authToken, topic })
+        setIsInput(false)
         if (results.isSuccess) {
             dispatch(addTopic(results.data))
         }
-    };
-
-    const handleSetInput = () => {
-        setIsInput(false);
-    }
+    };  
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-row justify-between gap-2 items-center pt-1 pb-1 pr-3 pl-3 ">
             <input
+                required
                 name="topic"
                 className="bg-gray-950 bg-opacity-40 p-1 rounded-l-lg pl-3"
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
             />
-            <button onClick={handleSetInput} className="bg-gray-950 bg-opacity-40 p-1 pl-2 pr-2 rounded-r-lg">Add</button>
+            <button className="bg-gray-950 bg-opacity-40 p-1 pl-2 pr-2 rounded-r-lg">Add</button>
         </form>
     );
 }
