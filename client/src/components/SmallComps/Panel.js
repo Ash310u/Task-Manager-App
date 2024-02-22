a97c83b612e4a9607949095b38894de688374e1f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-23 01:58:15 +0530

    feat: Add three new btns with styling

--- client/src/components/SmallComps/Panel.js
+++ client/src/components/SmallComps/Panel.js
@@ -1,7 +1,23 @@
+import { BiSolidEditAlt, BiSolidAddToQueue, } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+
 const Panel = ({ header, children }) => {
     return (
         <div className="w-80 m-1">
-            <h3 className="pl-6 pt-2 pb-2">{header}</h3>
+            <div className="pl-4 pr-4 pt-2 pb-2 flex flex-row justify-between items-center">
+                <h3 className="pl-1 pr-1 text-lg">{header}</h3>
+                <div className="flex flex-row justify-center items-center text-xl rounded-lg">
+                    <div className=" bg-black p-1.5 hover:bg-gray-800  rounded-l-lg">
+                        <BiSolidEditAlt />
+                    </div>
+                    <div className=" bg-black p-1.5 hover:bg-gray-800">
+                        <BiSolidAddToQueue />
+                    </div>
+                    <div className=" bg-black p-1.5 hover:bg-gray-800  rounded-r-lg">
+                        <BsThreeDots />
+                    </div>
+                </div>
+            </div>
             <div className="pb-4 pt-4 gap-2 flex flex-col items-center">
                 {children}
             </div>
