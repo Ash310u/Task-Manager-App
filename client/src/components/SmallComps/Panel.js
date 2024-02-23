96352feecf6db2c1a820a6ab8b5de1c833ba2961
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-23 14:31:38 +0530

    feat: set Short Cut to create task

--- client/src/components/SmallComps/Panel.js
+++ client/src/components/SmallComps/Panel.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { BiSolidEditAlt, BiSolidAddToQueue } from "react-icons/bi";
 import { BsThreeDots } from "react-icons/bs";
 import { BiX } from "react-icons/bi";
@@ -6,10 +6,25 @@ import IconDiv from "./utilsComp/IconDiv";
 
 const Panel = ({ header, children, }) => {
     const [isInputVisible, setIsInputVisible] = useState(false)
+    const [value, setValue] = useState('')
+    
+    const inputRef = useRef(null)
 
     const handleAddInput = () => {
         setIsInputVisible(curr => !curr)
     }
+
+    const handleSubmitTask = (e) => {        
+        setValue(e?.target.value)
+    }
+    
+    const handleEnterPress = (e) => {
+        if(e.key === 'Enter') {
+            e.preventDefault()
+            console.log(e.target.value)
+        }
+    }
+
     return (
         <div className="min-w-72 m-1">
             <div className="pt-2 pb-2 pl-1 pr-1 flex flex-row justify-between items-center select-none">
@@ -28,7 +43,18 @@ const Panel = ({ header, children, }) => {
             </div>
             <div className="pb-4 pt-4 gap-2 flex flex-col items-center">
                 {children}
-                {isInputVisible && <input type="text" placeholder="Add..." className="outline-none select-none ml-5 text-gray-50 min-w-64 max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3" />}
+                {
+                    isInputVisible &&
+                    <input 
+                        type="text"
+                        placeholder="Add..."
+                        value={value}
+                        ref={inputRef}
+                        onChange={handleSubmitTask}
+                        onKeyPress={handleEnterPress}
+                        className="outline-none select-none ml-5 text-gray-50 min-w-64 max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3" 
+                        />
+                }
             </div>
         </div>
     )
