94488b64bd4064c08186c5b1f007926fcae444d9
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-25 00:18:17 +0530

    feat: Add twmerge library

--- client/src/components/SmallComps/utilsComp/InputOperation.js
+++ client/src/components/SmallComps/utilsComp/InputOperation.js
@@ -1,9 +1,11 @@
-const InputOperation = ({...rest}) => {
+import { twMerge } from "tailwind-merge";
+
+const InputOperation = ({className, ...rest}) => {
     return (
         <input
             type="text"
             placeholder="Add..."
-            className="outline-none select-none ml-5 text-gray-50 min-w-64 max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3"
+            className={twMerge("outline-none select-none ml-5 text-gray-50 min-w-64 max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3", className)}
             {...rest}
         />
     )
