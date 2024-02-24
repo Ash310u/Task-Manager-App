32af3e8c1ff1f5c5595eef604bfe44dca2fe8efc
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-24 21:11:01 +0530

    feat: change border

--- client/src/components/SmallComps/ControlPanel.js
+++ client/src/components/SmallComps/ControlPanel.js
@@ -3,12 +3,12 @@ import EditInput from './EditInput'
 
 const ControlPanel = ({addTopic}) => {
     return (
-        <div className="flex flex-row items-center justify-around bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-4 gap-4">
-            <div className="pr-3 pl-3 pt-1 pb-1 flex flex-row gap-2 overflow-auto items-center bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg">
+        <div className="flex flex-row items-center justify-around bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 p-4 gap-4 rounded-2xl">
+            <div className="pr-3 pl-3 pt-1 pb-1 flex flex-row gap-2 overflow-auto items-center bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-2xl">
                 <StatusBox/>
                 <StatusBox/>
             </div>
-            <div className="flex flex-row items-center  gap-1 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg pt-2 pb-2 pr-3 pl-3 " >
+            <div className="flex flex-row items-center  gap-1 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 pt-2 pb-2 pr-3 pl-3 rounded-2xl" >
                 <EditInput addTopic={addTopic}/> 
             </div>
             {/* Style for hiding the scroll bar */}
