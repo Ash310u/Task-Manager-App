f201287f42733b380587325b972ffbb243a01890
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-25 01:57:03 +0530

    feat: setup the editInput to update the topic and set 'handleUpdateTopic' to onClick in a more optimized way

--- client/src/components/SmallComps/Panel.js
+++ client/src/components/SmallComps/Panel.js
@@ -5,10 +5,29 @@ import { BiX } from "react-icons/bi";
 import IconDiv from "./utilsComp/IconDiv";
 import InputOperation from "./utilsComp/InputOperation";
 
-const Panel = ({ header, children, onClick, onSubmit }) => {
+const Panel = ({ header, children, onClick, onTaskSubmit, onTopicSubmit }) => {
     const [isInputVisible, setIsInputVisible] = useState(false)
     const [isEditVisible, setIsEditVisible] = useState(false)
+
     const [taskValue, setTaskValue] = useState('')
+    const [topicValue, setTopicValue] = useState(header)
+
+    const handleEditInput = () => {
+        setIsEditVisible(curr => !curr)
+    }
+
+    const handleTopicInputChange = (e) => {
+        setTopicValue(e.target.value)
+    }
+
+    const handleTopicSubmitEnterPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault()
+            onTopicSubmit(topicValue)
+            setIsEditVisible(false)
+            setTopicValue('')
+        }
+    }
 
     const handleAddInput = () => {
         setIsInputVisible(curr => !curr)
@@ -21,27 +40,33 @@ const Panel = ({ header, children, onClick, onSubmit }) => {
         setTaskValue(e.target.value)
     }
 
-    const handleTaskInputClick = () => {
-        onClick()
-    }
-
     const handleTaskSubmitEnterPress = (e) => {
         if (e.key === 'Enter') {
             e.preventDefault()
-            onSubmit(taskValue)
+            onTaskSubmit(taskValue)
             setIsInputVisible(false)
             setTaskValue('')
         }
     }
 
-    const handleEditInput = () => {
-        setIsEditVisible(curr => !curr)
+    const handleInputClick = () => {
+        onClick()
     }
 
     return (
-        <div className="w-72 m-1">
-            <div className="pt-2 pb-2 pl-1 pr-1 flex flex-row justify-between items-center select-none">
-                {isEditVisible ? <InputOperation className={"min-w-40 m-2 p-1.5 pl-2 text-sm"}/> : <h3 className="text-base font-light pr-1.5 pl-1.5 subpixel-antialiased break-words select-text overflow-hidden">{header}</h3>}
+        <div className="w-72 m-1 flex flex-col" onClick={handleInputClick}>
+            <div className="pt-2 pb-2 pl-1 pr-1 flex flex-row justify-between items-center select-none" >
+                {
+                    isEditVisible ?
+                        <InputOperation
+                            maxLength={30}
+                            value={topicValue}
+                            onChange={handleTopicInputChange}
+                            onKeyPress={handleTopicSubmitEnterPress}
+                            className={"min-w-40 ml-0 mr-1 p-1 pl-1.5"}
+                        />
+                        : <h3 className="text-base font-light pl-1.5 pr-1.5 subpixel-antialiased break-words select-text overflow-hidden">{header}</h3>
+                }
                 <div className="gap-1 flex flex-row justify-center items-center text-xl rounded-lg">
                     <IconDiv onClick={handleEditInput}>
                         {isEditVisible ? <BiX /> : <BiSolidEditAlt />}
@@ -54,7 +79,7 @@ const Panel = ({ header, children, onClick, onSubmit }) => {
                     </IconDiv>
                 </div>
             </div>
-            <div className="pb-4 pt-4 gap-2 flex flex-col items-center" onClick={handleTaskInputClick}>
+            <div className="pb-4 pt-4 gap-2 flex flex-col items-center">
                 {children}
                 {isInputVisible && <InputOperation value={taskValue} onChange={handleTaskInputChange} onKeyPress={handleTaskSubmitEnterPress} />}
             </div>
