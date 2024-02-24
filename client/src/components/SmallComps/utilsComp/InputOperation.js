226d696019fe4ee282a4fc7e232a218584475f1c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-25 01:51:29 +0530

    feat: change font size

--- client/src/components/SmallComps/utilsComp/InputOperation.js
+++ client/src/components/SmallComps/utilsComp/InputOperation.js
@@ -5,7 +5,7 @@ const InputOperation = ({className, ...rest}) => {
         <input
             type="text"
             placeholder="Add..."
-            className={twMerge("outline-none select-none ml-5 text-gray-50 min-w-64 max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg p-3", className)}
+            className={twMerge("outline-none select-none ml-5 text-gray-50 min-w-64 max-h-min bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 font-light rounded-lg p-3", className)}
             {...rest}
         />
     )
