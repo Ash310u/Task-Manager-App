5f6481929a5138aa279bb5469e2690754dfabb4f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-29 19:16:59 +0530

    feat: set up 'statusContent' func and iterate through the prop data and rendered on StatusBox Comp

--- client/src/components/SmallComps/ControlPanel.js
+++ client/src/components/SmallComps/ControlPanel.js
@@ -1,12 +1,24 @@
 import StatusBox from "./StatusBox";
 import EditInput from './EditInput'
 
-const ControlPanel = ({addTopic}) => {
+const ControlPanel = ({ addTopic, data }) => {
+
+    const statusContent = data.map((topic) => {
+        const { title, _id} = topic;
+        const completedTask = topic?.tasks.filter((task) => {
+            return task.completed === true;
+        })
+        return (
+            <StatusBox key={_id} totalTasks={topic.tasks.length} completedTasks={completedTask.length}>
+                {title}
+            </StatusBox>
+        )
+    })
+
     return (
         <div className="flex flex-row items-center justify-around bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 p-4 gap-4 rounded-2xl">
             <div className="pr-3 pl-3 pt-1 pb-1 flex flex-row gap-2 overflow-auto items-center bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-2xl">
-                <StatusBox/>
-                <StatusBox/>
+                {statusContent}
             </div>
             <div className="flex flex-row items-center  gap-1 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 pt-2 pb-2 pr-3 pl-3 rounded-2xl" >
                 <EditInput addTopic={addTopic}/> 
