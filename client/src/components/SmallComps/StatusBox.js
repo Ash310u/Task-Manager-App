8ca09de4207ac4348f843bba0e035c66a582425d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-03-01 19:00:08 +0530

    feat: render the percentage value of task completion using 'percentage' prop

--- client/src/components/SmallComps/StatusBox.js
+++ client/src/components/SmallComps/StatusBox.js
@@ -1,11 +1,11 @@
-const StatusBox = ({children, totalTasks, completedTasks}) => {
+const StatusBox = ({ children, percentage }) => {
     return (
         <div className="flex flex-col min-w-32 bg-gray-950 p-1 pr-3 pl-3 bg-opacity-30 rounded-xl">
             <div className="">
                 {children}
             </div>
             <div className="max-h-6">
-                {completedTasks}/{totalTasks}
+                {percentage}%
             </div>
         </div>
     )
