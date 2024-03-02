ce62f6c9fd3066f0e8e70347c1fe56051aee2813
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-03-02 15:05:41 +0530

    styling: Some styling changes

--- client/src/components/SmallComps/StatusBox.js
+++ client/src/components/SmallComps/StatusBox.js
@@ -1,12 +1,24 @@
 const StatusBox = ({ children, percentage }) => {
     return (
-        <div className="flex flex-col min-w-32 bg-gray-950 p-1 pr-3 pl-3 bg-opacity-30 rounded-xl">
-            <div className="">
+        <div className="text-green-200 opacity-70 text-sm font-extralight flex gap-1 flex-col justify-center h-2/3 min-w-36 p-1 bg-opacity-30 rounded-md">
+            <div className="flex items-center bg-gray-100 bg-opacity-10 pl-1.5 pr-1.5 rounded-sm overflow-auto" >
                 {children}
             </div>
-            <div className="max-h-6">
+            <div className="flex items-center bg-gray-100 bg-opacity-10 pl-1.5 pr-1.5 rounded-sm ">
                 {percentage}%
             </div>
+            {/* Style for hiding the scroll bar */}
+            <style>
+                {`
+                    .overflow-auto::-webkit-scrollbar {
+                        width: 0 !important;
+                    }
+                    .overflow-auto {
+                        scrollbar-width: none;
+                        -ms-overflow-style: none;
+                    }
+                `}
+            </style>
         </div>
     )
 }
