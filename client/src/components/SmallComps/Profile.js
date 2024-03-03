44150c4ecb56b5fd729d89bfe354bd74ebac9a8f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-03-03 05:32:13 +0530

    feat: Add Profile comp

---
+++ client/src/components/SmallComps/Profile.js
@@ -0,0 +1,14 @@
+const Profile = () => {
+    return (
+        <div className="flex flex-row items-center justify-between m-5 p-3 w-48 h-16 text-gray-200 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 rounded-lg">
+            <div className="w-10 aspect-square rounded-full bg-login bg-cover bg-center">
+
+            </div>
+            <div className="opacity-70">
+                Ashutosh Saha
+            </div>
+        </div>
+    )    
+}
+
+export default Profile;
