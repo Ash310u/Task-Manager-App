0536b2b7da6107df4d9483629c011ad661890162
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-04-07 22:10:36 +0530

    refactor: controlPanel

--- client/src/components/SmallComps/ControlPanel.js
+++ client/src/components/SmallComps/ControlPanel.js
@@ -2,31 +2,43 @@ import StatusBox from "./StatusBox";
 import EditInput from './EditInput'
 import { stateAddTopic, useCreateTopicMutation } from "../../store";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const ControlPanel = ({ data }) => {
     const dispatch = useDispatch()
+    // const { tasks } = useSelector((state) => {
+    //     return state.taskSlice
+    // })
+
     const authToken = window.localStorage.getItem('authToken')
 
     const [createTopic, results] = useCreateTopicMutation()
 
     useEffect(() => {
-        if(results.isSuccess) {
+        if (results.isSuccess) {
             dispatch(stateAddTopic(results.data))
         }
-    },[results.data, dispatch])
+    }, [results.data, dispatch])
 
     const handleAddTopic = (topic) => {
         createTopic({ authToken, topic })
     }
+
     // const statusContent = data.map((topic) => {
     //     const { title, _id } = topic;
-    //     const completedTask = topic?.tasks.filter((task) => {
-    //         return task.completed === true;
-    //     })
+    //     let total = 0;
+    //     let state = 0
+    //     for (let t = 0; t < tasks.length; t++) {
+    //         if (topic._id === tasks[t].parent_id) {
+    //             if (tasks[t].completed === true) {
+    //                 state = state++ ;
+    //                 total = t;
+    //             }
+    //         }
+    //     }
 
     //     let taskCompletion
-    //     completedTask.length !== 0 ? taskCompletion = Math.round((completedTask.length/topic.tasks.length) * 100) : taskCompletion = 0
+    //     state !== 0 ? taskCompletion = Math.round((state / total) * 100) : taskCompletion = 0
 
     //     return (
     //         <StatusBox key={_id} percentage={taskCompletion}>
@@ -41,7 +53,7 @@ const ControlPanel = ({ data }) => {
                 {/* {statusContent} */}
             </div>
             <div className="flex flex-row items-center  gap-1 bg-gray-200 border-gray-200 backdrop-blur-lg bg-opacity-10 p-2 pr-1 pl-1 rounded-3xl" >
-                <EditInput addTopic={handleAddTopic}/> 
+                <EditInput addTopic={handleAddTopic} />
             </div>
             {/* Style for hiding the scroll bar */}
             <style>
